ce7122adb5a5966afd6bda3b835e4056c4d51f60
ElvinYekayev <elvinyekay@gmail.com>
2025-08-04 23:46:55 +0400

    fix:compile-error-hero-1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import { Inter_Tight } from 'next/font/google';
 import './globals.css';
 import Footer from '@/sections/Footer';
 import Header from '@/sections/Header';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 const interTight = Inter_Tight({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'], // İstədiyin çəkilər
