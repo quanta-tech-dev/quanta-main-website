50245057ee8d66e119f37b5bcad20502b29ffa96
elvinyekayev <elvinyekay@gmail.com>
2025-09-01 14:22:25 +0400

    created dashboard layout

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -1,15 +1,10 @@
 "use client";
-import {ThemeProvider} from "@/app/(admin)/dashboard/context/ThemeContext";
-import {SidebarProvider} from "@/app/(admin)/dashboard/context/SidebarContext";
-import Content from "@/app/(admin)/dashboard/layout/Content";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
 
     return (
-        <ThemeProvider>
-            <SidebarProvider >
-                <Content>{children}</Content>
-            </SidebarProvider>
-        </ThemeProvider>
+        <main>
+            {children}
+        </main>
     );
 }
--- src/components/HeaderLogo.tsx
+++ src/components/HeaderLogo.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import React from 'react';
+import Image from "next/image";
 
 const HeaderLogo = () => {
   return (
     <div className="flex-shrink-0 flex items-center">
       <Link href="/" className="flex items-center">
-        <img className="h-16 w-auto" src="/logo.svg" alt="Logo" />
+        <Image className="h-16 w-auto" src="/logo.svg" alt="Logo" />
         {/* <span className="ml-2 text-xl font-bold text-gray-900">Brand</span> */}
       </Link>
     </div>
