77910810c8c409bbb2e17a4d21a7d6edcc08bb72
elvinyekayev <elvinyekay@gmail.com>
2025-09-01 14:24:22 +0400

    created dashboard layout

--- src/components/HeaderLogo.tsx
+++ src/components/HeaderLogo.tsx
@@ -6,7 +6,7 @@ const HeaderLogo = () => {
   return (
     <div className="flex-shrink-0 flex items-center">
       <Link href="/" className="flex items-center">
-        <Image className="h-16 w-auto" src="/logo.svg" alt="Logo" />
+        <Image className="h-16 w-auto" src="/logo.svg" alt="Logo" width={256} height={256} />
         {/* <span className="ml-2 text-xl font-bold text-gray-900">Brand</span> */}
       </Link>
     </div>
